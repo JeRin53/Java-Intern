edd6485d73698cbd54eabfadb97b11049ade68da
Jerin Joejoe <149098496+JeRin53@users.noreply.github.com>
2026-03-19 17:37:42 +0530

    Add files via upload

---
+++ sampleProject/src/main/java/com/opentrends/sampleProject/Service/DepartmentService.java
@@ -0,0 +1,24 @@
+package com.opentrends.sampleProject.Service;
+
+import com.opentrends.sampleProject.Dto.DepartmentDtoProjection;
+import com.opentrends.sampleProject.Model.Department;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface DepartmentService {
+
+    Department saveDepartment(Department department);
+
+    List<Department> fetchDepartment();
+
+    Page<Department> fetchDepartmentWithPagination(int page, int size, String sortBy);
+
+    Department updateDepartment(Department department, Long departmentID);
+
+    String deleteDepartment(Long departmentId);
+
+    List<Department> findDepartmentByName(String name);
+
+    List<DepartmentDtoProjection> getDepartmentDtoProjection();
+}
